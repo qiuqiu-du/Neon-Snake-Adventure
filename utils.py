5fde3386837faf28cbe47fb3e751a070e344e98f
QIU <qiuqiudu@protonmail.com>
2025-04-24 01:36:18 +0800

    Switch input method automatically

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import ctypes
+
 def load_high_score():
     try:
         with open("highscore.txt", "r") as f:
@@ -9,3 +11,22 @@ def load_high_score():
 def save_high_score(score):
     with open("highscore.txt", "w") as f:
         f.write(str(score))
+
+
+def set_english_input():
+    try:
+        hwnd = ctypes.windll.user32.GetForegroundWindow()
+
+        # English-America 0x0409
+        ctypes.windll.user32.PostMessageW(hwnd, 0x50, 0, 0x0409)
+
+    except :
+        try:
+            # Chinese (Simplified China) 0x0804
+            ctypes.windll.user32.PostMessageW(hwnd, 0x50, 0, 0x0804)
+
+            # Shift
+            ctypes.windll.user32.keybd_event(0xA0, 0, 0, 0)
+            ctypes.windll.user32.keybd_event(0xA0, 0, 2, 0)
+        except:
+            pass
